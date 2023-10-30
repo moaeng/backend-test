5590b4172638f2d757ba99badfba1252f9001cb6
Moa Englander <moaenglander@gmail.com>
2023-10-30 14:10:14 +0100

    New backend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
-const app = express(),
-  port = process.env.PORT || 3000
+const cors = require("cors")
+const app = express()
+port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get("/", (_request, response) => {
   response.send({ hello: "Mathias" })
